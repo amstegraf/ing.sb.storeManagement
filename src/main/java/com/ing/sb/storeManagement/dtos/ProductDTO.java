34252b69ce702674a2f68bd440ca5995fb874871
Aurelian Apostol <aurelian.apostol@gmail.com>
2022-03-30 21:57:17 +0300

    added entity and dto classes for products

---
+++ src/main/java/com/ing/sb/storeManagement/dtos/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.ing.sb.storeManagement.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class ProductDTO {
+    private String id;
+    @NotEmpty(message = "Title can't be empty")
+    private String title;
+    private String description;
+
+    private LocalDate dateAdded;
+    private LocalDateTime lastModified;
+}
---
+++ src/main/java/com/ing/sb/storeManagement/entities/Product.java
@@ -0,0 +1,49 @@
+package com.ing.sb.storeManagement.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = Product.TABLE_NAME)
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Data
+public class Product {
+    public static final String TABLE_NAME = "products";
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date_added")
+    @CreatedDate
+    private LocalDate dateAdded;
+
+    @Column(name = "last_modified")
+    @LastModifiedDate
+    private LocalDateTime lastModified;
+}
