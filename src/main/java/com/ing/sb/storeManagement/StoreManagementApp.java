f3e62184da515a58cee3c3810477d61fad864f70
Aurelian Apostol <aurelian.apostol@gmail.com>
2022-03-30 20:04:32 +0300

    initial commit

---
+++ src/main/java/com/ing/sb/storeManagement/StoreManagementApp.java
@@ -0,0 +1,11 @@
+package com.ing.sb.storeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementApp.class);
+    }
+}
