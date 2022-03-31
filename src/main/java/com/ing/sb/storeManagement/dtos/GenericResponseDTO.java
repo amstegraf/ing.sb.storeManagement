53fb0e12f306c7edbef42c4c7d2d952b3e82fe70
Aurelian Apostol <aurelian.apostol@gmail.com>
2022-03-31 14:44:42 +0300

    added correlationId in generic response for error handling

--- src/main/java/com/ing/sb/storeManagement/dtos/GenericResponseDTO.java
+++ src/main/java/com/ing/sb/storeManagement/dtos/GenericResponseDTO.java
@@ -3,17 +3,20 @@ package com.ing.sb.storeManagement.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.slf4j.MDC;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GenericResponseDTO {
+    private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
 
     Integer code;
     String message;
     Object body;
+    String correlationId;
 
     public GenericResponseDTO(Integer code, String message) {
-        this(code, message, null);
+        this(code, message, null, MDC.get(CORRELATION_ID_LOG_VAR_NAME));
     }
 }
